7e76d28d3aade69cd78520b44efe42efda723f77
ujangaja <ujangaja@gmail.com>
2021-08-16 17:25:13 +0700

    menangani router

--- app.js
+++ app.js
@@ -5,14 +5,20 @@ const express = require('express')
 
 const app = express()
 
-app.use((req,res,next)=>{
-    console.log('ini adalah middleware');
+app.use('/',(req,res,next)=>{
+    console.log('ini adalah middleware selalu berjalan');
     next()//digunakan untuk mengijikan source dibawahnya berjalan
 }) 
 
-app.use((req,res,next)=>{
+app.use('/student-add',(req,res,next)=>{
+    res.send('<h1>Page add student</h1>')
     console.log('ini  middleware');
 }) 
+
+app.use('/',(req,res,next)=>{
+    res.send('<h1>Ini adalah halaman home</h1>')
+}) 
+
 const server = http.createServer(app);
 
 server.listen(3000);
