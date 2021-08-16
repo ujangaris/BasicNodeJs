eb4d631895d13b67cd4d596877322cd3481addb8
ujangaja <ujangaja@gmail.com>
2021-08-16 17:16:20 +0700

    middleware

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-// 02. Install express js renyek
+// 02. Middleware
 const http = require("http");
 // const router = require('./routes')
 const express = require('express')
 
 const app = express()
 
+app.use((req,res,next)=>{
+    console.log('ini adalah middleware');
+    next()//digunakan untuk mengijikan source dibawahnya berjalan
+}) 
+
+app.use((req,res,next)=>{
+    console.log('ini  middleware');
+}) 
 const server = http.createServer(app);
 
 server.listen(3000);
