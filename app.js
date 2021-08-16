79c709b7a255717493d33bc78ff61603d6c40d53
ujangaja <ujangaja@gmail.com>
2021-08-16 17:42:15 +0700

    belajar parsing data dengan body-parser

--- app.js
+++ app.js
@@ -2,15 +2,22 @@
 const http = require("http");
 // const router = require('./routes')
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
-const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended:false}))
 
 
 app.use('/student-add',(req,res,next)=>{
     res.send('<form action="/student" method="POST" ><input type="text" name="student"><button type="submit">SEND</button></form>')
     console.log('ini  middleware');
 }) 
+
+app.use('/student',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/')
+})
 
 app.use('/',(req,res,next)=>{
     res.send('<h1>Page Home</h1>')
