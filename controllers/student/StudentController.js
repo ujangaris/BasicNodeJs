32804fba7e93505e67291a7a6d880b1811fc2445
ujangaja <ujangaja@gmail.com>
2021-08-20 08:55:21 +0700

    session 6 | 03 Simpan data dalam file JSON

--- controllers/student/StudentController.js
+++ controllers/student/StudentController.js
@@ -1,3 +1,5 @@
+const StudentModel = require("../../models/student/studentModel");
+
 exports.getStudent = (req, res, next) => {
   res.render("student/student-list", {
     pageTitle: "Student List",
@@ -5,7 +7,17 @@ exports.getStudent = (req, res, next) => {
   });
 };
 
-exports.PostAddStudent = (req, res, next) => {};
+exports.PostAddStudent = (req, res, next) => {
+  const name = req.body.name;
+  const classs = req.body.classs;
+  const nik = req.body.nik;
+  const Image = req.body.Image;
+  const gender = req.body.gender;
+  const Address = req.body.Address;
+  const Student = new StudentModel(name, classs, nik, Image, gender, Address);
+  Student.save();
+  res.redirect("/");
+};
 
 exports.getAddStudent = (req, res, next) => {
   res.render("student/student-add", {
---
+++ models/student/studentModel.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const fs = require("fs");
+const { networkInterfaces } = require("os");
+
+module.exports = class Student {
+  constructor(name, classs, nik, Image, gender, Address) {
+    this.name = name;
+    this.classs = classs;
+    this.nik = nik;
+    this.Image = Image;
+    this.gender = gender;
+    this.Address = Address;
+  }
+  save() {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "student.json"
+    );
+    let student = [];
+    fs.readFile(p, (err, fileContent) => {
+      if (!err) {
+        student = JSON.parse(fileContent);
+      }
+      student.push(this);
+      fs.writeFile(p, JSON.stringify(student), (err) => {
+        console.log(err);
+      });
+    });
+  }
+};
